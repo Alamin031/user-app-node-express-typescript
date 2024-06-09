47e9989edc1ee74342c6187c1bc21673c432b301
Md. Al Amin Chowdhury <94056482+Alamin031@users.noreply.github.com>
2024-06-09 23:49:26 +0600

    Update bcrypt dependency to version 5.1.1

--- dist/services/user.service.js
+++ dist/services/user.service.js
@@ -1,27 +1,4 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -31,10 +8,13 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.UserService = void 0;
 const user_entity_1 = require("../models/user.entity");
-const bcrypt = __importStar(require("bcrypt"));
+const bcrypt_1 = __importDefault(require("bcrypt"));
 class UserService {
     getUserProfile(userId) {
         return __awaiter(this, void 0, void 0, function* () {
@@ -59,8 +39,8 @@ class UserService {
                     throw new Error("User not found");
                 }
                 if (data.password) {
-                    const salt = yield bcrypt.genSalt();
-                    data.password = yield bcrypt.hash(data.password, salt);
+                    const salt = yield bcrypt_1.default.genSalt();
+                    data.password = yield bcrypt_1.default.hash(data.password, salt);
                 }
                 const updatedUserProfile = yield user_entity_1.User.findByIdAndUpdate(userId, data, {
                     new: true,
